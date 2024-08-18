c2f637a81704c9fa4ba6e1d3f3fdc27aca8261e4
Hunter Wang <s092284s@gmail.com>
2024-08-18 18:57:05 +0800

    create new meeting

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import Image from "next/image";
+import HomeCard from "./HomeCard";
+import { useReducer, useState } from "react";
+import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "@/components/ui/use-toast";
+
+const MeetingTypeList = () => {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetails, setCallDetails] = useState<Call>();
+
+  const createMeeting = async () => {
+    if (!user || !client) return;
+
+    try {
+      if (!values.dateTime) {
+        toast({
+          title: "Please select a date and time",
+        });
+        return;
+      }
+
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+
+      toast({
+        title: "Meeting Created",
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed to create meeting",
+      });
+    }
+  };
+
+  return (
+    <section className=" grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="建立即時會議"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        className="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="加入即時會議"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="建立會議排程"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="查看會議錄影"
+        handleClick={() => router.push("/recordings")}
+        className="bg-yellow-1"
+      />
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
