1530346bfa62ec3573f2c9ccac02a5ce56a6ed0a
Hunter Wang <s092284s@gmail.com>
2024-10-08 22:10:41 +0800

    delete fake avatars

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -28,6 +28,7 @@ const MeetingCard = ({
   link,
 }: MeetingCardProps) => {
   const { toast } = useToast();
+
   return (
     <section className=" flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
       <article className=" flex flex-col gap-5">
@@ -41,7 +42,8 @@ const MeetingCard = ({
       </article>
       <article className={cn("flex justify-center relative", {})}>
         <div className=" relative flex w-full max-sm:hidden">
-          {avatarImages.map((img, index) => (
+          {/* TODO: get participants img  */}
+          {/* {avatarImages.map((img, index) => (
             <Image
               key={index}
               src={img}
@@ -51,10 +53,10 @@ const MeetingCard = ({
               className={cn("rounded-full", { absolute: index > 0 })}
               style={{ top: 0, left: index * 28 }}
             />
-          ))}
-          <div className=" flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
+          ))} */}
+          {/* <div className=" flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
             +5
-          </div>
+          </div> */}
         </div>
         {!isPreviousMeeting && (
           <div className="flex gap-2">
